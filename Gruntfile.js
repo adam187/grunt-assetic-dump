25a7d94c1ff81395f1fe51fc7716345f3cb9a236
Adam Misiorny <adam.misiorny@gmail.com>
2013-12-16 18:17:03 +0100

    update based on template

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,10 +10,8 @@
 
 module.exports = function(grunt) {
 
-  // Configuration.
+  // Project configuration.
   grunt.initConfig({
-
-    // Jshint
     jshint: {
       all: [
         'Gruntfile.js',
@@ -25,31 +23,35 @@ module.exports = function(grunt) {
       },
     },
 
-    assetic_dump: {
-    },
-
     // Before generating any new files, remove any previously-created files.
     clean: {
       tests: ['tmp'],
     },
 
+    // Configuration to be run (and then tested).
+    assetic_dump: {
+    },
+
     // Unit tests.
     nodeunit: {
-      tests: ['tests/*_test.js'],
+      tests: ['test/*_test.js'],
     },
 
   });
 
-  // Load this plugin's task.
+  // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // Load deps tasks
+  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-nodeunit');
 
-  // Register tasks
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
   grunt.registerTask('test', ['clean', 'assetic_dump', 'nodeunit']);
+
+  // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
 
 };
